31f2cabd1e59904b08fa9d418db7efdf312a75c8
LeeJaeRyun <pinixs3@naver.com>
2025-08-07 08:04:40 +0900

    1929 - refactor

--- 1st_week/1929.py
+++ 1st_week/1929.py
@@ -1,15 +1,10 @@
 m, n = map(int, input().split())
 
 for num in range(m, n+1):
-    if num < 2:
+    if num == 1:
         continue
-    if num == 2:
-        print(2)
-        continue
-    is_prime = True
     for i in range(2, int(num**0.5)+1):
         if num % i == 0:
-            is_prime = False
             break
-    if is_prime:
+    else:
         print(num)
