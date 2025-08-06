ce2152d63895d0d5b2c598dc64105584b1f28910
LeeJaeRyun <pinixs3@naver.com>
2025-08-07 03:31:25 +0900

    1929

---
+++ 1st_week/1929.py
@@ -0,0 +1,15 @@
+m, n = map(int, input().split())
+
+for num in range(m, n+1):
+    if num < 2:
+        continue
+    if num == 2:
+        print(2)
+        continue
+    is_prime = True
+    for i in range(2, int(num**0.5)+1):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(num)
