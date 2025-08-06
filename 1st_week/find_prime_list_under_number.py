c5de0403fd5cdb191e89f07441d64f800f42a0bd
LeeJaeRyun <pinixs3@naver.com>
2025-08-07 08:03:34 +0900

    소수구하기 - 2차 개선

--- 1st_week/find_prime_list_under_number.py
+++ 1st_week/find_prime_list_under_number.py
@@ -5,7 +5,7 @@ def find_prime_list_under_number(number):
     result = []
     for num in range(2, number+1): #2부터 number까지 반복
         for i in result:#소수로만 구성, num=2일 경우 result는 비었기 때문에 바로 result 리스트에 2값이 추가됌
-            if num%i == 0:
+            if i*i <= num and num%i == 0:
                 break
         else:
             result.append(num)
