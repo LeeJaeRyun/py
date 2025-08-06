08751c92532f936253f676531007e2385417869a
LeeJaeRyun <pinixs3@naver.com>
2025-08-07 06:53:01 +0900

    소수구하기 - for else 적용

--- 1st_week/find_prime_list_under_number.py
+++ 1st_week/find_prime_list_under_number.py
@@ -3,17 +3,11 @@ input = 20
 
 def find_prime_list_under_number(number):
     result = []
-    if number < 2:
-        return []
-    if number == 2:
-        return [2]
     for num in range(2, number+1): #2부터 number까지 반복
-        is_prime = True
         for i in range(2,num):#2부터 num-1까지 반복
             if num%i == 0:
-                is_prime = False
                 break
-        if is_prime == True:
+        else:
             result.append(num)
 
     return result
