58d24da86d112adacdbfc1d384615394bc8ab032
LeeJaeRyun <pinixs3@naver.com>
2025-08-18 19:47:22 +0900

    print linked list Kth value

--- 2nd_week/HW1.py
+++ 2nd_week/HW1.py
@@ -15,16 +15,15 @@ class LinkedList:
         cur.next = Node(value)
 
     def get_kth_node_from_last(self, k):
-        length = 1
-        cur = self.head
-        while cur.next is not None:
-            cur = cur.next
-            length += 1
-        end_length = length - k
-        cur = self.head
-        for i in range(end_length):
-            cur = cur.next
-        return cur
+        slow = self.head
+        fast = self.head
+
+        for i in range(k):
+            fast = fast.next
+        while fast is not None:
+            slow = slow.next
+            fast = fast.next
+        return slow
 
 
 
